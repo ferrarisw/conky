8db67e934d97e337e10bb1def9fcc00e05d0ccdb
Davide Ferrari <davideferrari8@gmail.com>
2018-01-08 17:15:19 +0100

    Minor graphical improvements

--- conky-startup.sh
+++ conky-startup.sh
@@ -2,14 +2,14 @@
 
 # http://conky.sourceforge.net/config_settings.html
 # http://conky.sourceforge.net/variables.html
-
+# https://github.com/ferrarisw/conky
 killall conky
 
-#sleep 10 &
+sleep 10 &
 conky -c $HOME/workspace/conky/I-system &
-#sleep 1 &
+sleep 1
 conky -c $HOME/workspace/conky/I-clock &
-#sleep 1 &
+sleep 1
 conky -c $HOME/workspace/conky/I-network &
 
 exit
