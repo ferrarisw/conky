ac80e482ed57e525cb58cf5bd05308dc0d31fe8d
Davide Ferrari <davideferrari8@gmail.com>
2018-01-09 11:53:11 +0100

    Updated References and script timeout

--- conky-startup.sh
+++ conky-startup.sh
@@ -3,9 +3,10 @@
 # http://conky.sourceforge.net/config_settings.html
 # http://conky.sourceforge.net/variables.html
 # https://github.com/ferrarisw/conky
+
 killall conky
 
-sleep 10 &
+sleep 1
 conky -c $HOME/workspace/conky/I-system &
 sleep 1
 conky -c $HOME/workspace/conky/I-clock &
